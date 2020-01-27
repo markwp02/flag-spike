f0de88b278a85aed8d6e8e8cb7b415f190a56aba
Mark <markwp02@gmail.com>
2020-01-27 19:09:36 +1100

    Iterates through all subdirectories of inbox
    Instead of hardcoding walking through Dir1 and Dir2, iterate through all subdirectories of Inbox

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("-----Move Inbox to Outbox------");
-        Path dir1Path = FileSystems.getDefault().getPath("Inbox", "Dir1");
-        Path dir2Path = FileSystems.getDefault().getPath("Inbox", "Dir2");
+        Path dirInboxPath;
+        Path dirOutboxPath;
+        Path inboxPath = FileSystems.getDefault().getPath("Inbox");
+        Path outboxPath = FileSystems.getDefault().getPath("Outbox");
+        DirectoryStream.Filter<Path> dirFilter = p -> Files.isDirectory(p);
 
-        Path outboxDir1 = FileSystems.getDefault().getPath("Outbox", "Dir1");
-        Path outboxDir2 = FileSystems.getDefault().getPath("Outbox", "Dir2");
-
-        try{
-            Files.walkFileTree(dir1Path, new MoveFiles(dir1Path, outboxDir1));
-            Files.walkFileTree(dir2Path, new MoveFiles(dir2Path, outboxDir2));
-        } catch (IOException i) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(inboxPath, dirFilter)){
+            for(Path dir: contents) {
+                System.out.println(dir.toAbsolutePath());
+                Path relatavisedPath = inboxPath.relativize(dir);
+                dirInboxPath = inboxPath.resolve(relatavisedPath);
+                dirOutboxPath = outboxPath.resolve(relatavisedPath);
+                Files.walkFileTree(dirInboxPath, new MoveFiles(dirInboxPath, dirOutboxPath));
+            }
+        } catch (IOException | DirectoryIteratorException i ) {
             System.out.println(i.getMessage());
         }
 
